ac2bd38be044e400f0ec774c4a2eb22aea971dbb
Ashutosh Pratap Singh <ashutoshpratap26@gmail.com>
2024-07-05 16:02:43 +0530

    first commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './Components/Weather'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
---
+++ weather-app/src/Components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'
+const Weather = () => {
+  return (
+    <div className='weather'>
+       <div className='search-bar'>
+        <input type="text" placeholder='Search'/>
+        <img src="" alt="" />
+        </div> 
+    </div>
+  )
+}
+
+export default Weather
